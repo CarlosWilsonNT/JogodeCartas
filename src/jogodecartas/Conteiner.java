92f7a11aea02f2ecaad73fc7ada1cbdc93904b80
CarlosWilsonNT <carloswilson223@gmail.com>
2024-11-23 14:43:46 -0300

    Update Conteiner.java

--- src/jogodecartas/Conteiner.java
+++ src/jogodecartas/Conteiner.java
@@ -2,29 +2,41 @@ package jogodecartas;
 
 import javax.swing.JFrame;
 import javax.swing.JScrollPane;
+
 import jogodecartas.Modelo.deckplayer1;
 import jogodecartas.Modelo.deckplayer2;
 import jogodecartas.Modelo.duelo;
+import jogodecartas.Modelo.telaInicial;
 
 public class Conteiner extends JFrame {
 
+    private telaInicial telaInicialPanel;
     private deckplayer1 deck1Panel;
     private deckplayer2 deck2Panel;
     private duelo dueloPanel;
     private JScrollPane scrollPane;
 
     public Conteiner() {
-        deck1Panel = new deckplayer1();
-
-        scrollPane = new JScrollPane(deck1Panel);
-        add(scrollPane);
-
+        telaInicialPanel = new telaInicial();
+        add(telaInicialPanel);
         setTitle("Arcana");
         setSize(1080, 728);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
         this.setResizable(false);
         setVisible(true);
+
+        telaInicialPanel.getIniciarButton().addActionListener(e -> irParaDeck1());
+    }
+
+    public void irParaDeck1() {
+        remove(telaInicialPanel);
+        deck1Panel = new deckplayer1();
+        scrollPane = new JScrollPane(deck1Panel);
+        add(scrollPane);
+        revalidate();
+        repaint();
+        deck1Panel.requestFocusInWindow();
     }
 
     public void irPraraDeck2() {
@@ -34,10 +46,9 @@ public class Conteiner extends JFrame {
         add(scrollPane);
         revalidate();
         repaint();
-
         deck2Panel.requestFocusInWindow();
     }
-    
+
     public void irPraraDuelo() {
         remove(scrollPane);
         dueloPanel = new duelo(deck1Panel.getDeck1(), deck2Panel.getDeck2());
